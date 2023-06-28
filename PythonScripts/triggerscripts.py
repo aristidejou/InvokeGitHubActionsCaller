9acdf552e0587d7dd731afb601ad6951e06cc5dd
aristidejou <106462048+aristidejou@users.noreply.github.com>
2023-06-28 18:32:18 -0500

    Update triggerscripts.py

--- PythonScripts/triggerscripts.py
+++ PythonScripts/triggerscripts.py
@@ -27,7 +27,5 @@ def trigger_workflow(Workflow_Name,parameter1,parameter2):
 
       responseValue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
       print(responseValue.content)
-       responseValue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
-      print("the response message is  ", responsevalue.content)
-
+     
 trigger_workflow(Workflow_Name,parameter1,parameter2)
